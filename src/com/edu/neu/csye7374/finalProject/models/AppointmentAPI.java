5d12bddb10988b3ddc46bc0b1c01e7d45c96f8a2
harish-gunda <gunda.sa@northeastern.edu>
2022-08-15 22:04:00 -0400

    adding admin initial commit

--- src/com/edu/neu/csye7374/finalProject/models/AppointmentAPI.java
+++ src/com/edu/neu/csye7374/finalProject/models/AppointmentAPI.java
@@ -3,6 +3,8 @@ package com.edu.neu.csye7374.finalProject.models;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.edu.neu.csye7374.finalProject.models.Appointment;
+
 public interface AppointmentAPI {
 	public Appointment createAppoinment(int doctorId, int patientId, LocalDateTime appointmentDateTime, String appointmentDescription, List<Appointment> appointments);
 }
--- src/com/edu/neu/csye7374/finalProject/models/AppointmentAdapter.java
+++ src/com/edu/neu/csye7374/finalProject/models/AppointmentAdapter.java
@@ -4,12 +4,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.edu.neu.csye7374.finalProject.factories.AppointmentFactory;
+import com.edu.neu.csye7374.finalProject.models.Appointment;
+import com.edu.neu.csye7374.finalProject.models.AppointmentAPI;
+import com.edu.neu.csye7374.finalProject.models.AppointmentCheckerAPI;
 
 public class AppointmentAdapter implements AppointmentAPI{
 	AppointmentCheckerAPI appointmentCheckerAPI;
 	public AppointmentAdapter(AppointmentCheckerAPI appointmentCheckerAPI) {
 		this.appointmentCheckerAPI = appointmentCheckerAPI;
 	}
+	
+//	@Override
 	public Appointment createAppoinment(int doctorId, int patientId, LocalDateTime appointmentDateTime, String appointmentDescription, List<Appointment> appointments) {
 		
 		if(appointmentCheckerAPI.checkAppointment(appointments, appointmentDateTime, doctorId)) {
