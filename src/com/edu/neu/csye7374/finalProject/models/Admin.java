b5343143fd0b2930c3e3fa746b3fb7c9c2c2c0ae
Chinmay Rout <thechinmayrout@gmail.com>
2022-08-15 20:02:11 -0400

    Added List of Doctors to Department

--- src/com/edu/neu/csye7374/finalProject/models/Admin.java
+++ src/com/edu/neu/csye7374/finalProject/models/Admin.java
@@ -1,10 +1,14 @@
 package com.edu.neu.csye7374.finalProject.models;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Admin {
     private String hospitalId;
     private String post;
     private List<Patient> listOfPatients = new ArrayList<Patient>();
+
+
+
 
 }
--- src/com/edu/neu/csye7374/finalProject/models/Department.java
+++ src/com/edu/neu/csye7374/finalProject/models/Department.java
@@ -1,9 +1,15 @@
 package com.edu.neu.csye7374.finalProject.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department {
     Admin admin;
     String departmentName;
     String departmentManager;
+
+    private List<Doctor> listOfDoctors = new ArrayList<Doctor>();
+
 
     public Department(DepartmentBuilder departmentBuilder) {
         super();
@@ -24,10 +30,15 @@ public class Department {
         return departmentManager;
     }
 
+    public List<Doctor> getListOfDoctors() {
+        return listOfDoctors;
+    }
+
     public static class DepartmentBuilder{
         Admin admin;
         String departmentName;
         String departmentManager;
+        List<Doctor> listOfDoctors = new ArrayList<Doctor>();
 
         public DepartmentBuilder setAdmin(Admin admin) {
             this.admin = admin;
@@ -41,6 +52,11 @@ public class Department {
 
         public DepartmentBuilder setDepartmentManager(String departmentManager) {
             this.departmentManager = departmentManager;
+            return this;
+        }
+
+        public DepartmentBuilder setListOfDoctors(List<Doctor> listOfDoctors) {
+            this.listOfDoctors = listOfDoctors;
             return this;
         }
 
